bff4a83393c951cef21217071ef6bcaebbcc5f8d
Goitsee2 <2560230@students.wits.ac.za>
2024-05-22 14:35:07 +0000

    r

--- mpi_trape_collectve_2603828.c
+++ mpi_trape_collectve_2603828.c
@@ -5,22 +5,15 @@
 
 int main(int argc, char *argv[])
 {
-    int num_procs, myrank, data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int num_procs, myrank;
     MPI_Init(&argc, &argv);
 
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    char g[12] = "Hello World!";
+    MPI_Bcast(g, 12, MPI_CHAR, 0, MPI_COMM_WORLD);
+    printf("Process %d received token %s from process %d.\n", myrank, g, 0);
 
-    printf("FROM process %d out of %d , Hello World!\n", myrank, num_procs);
-    if (myrank == 0)
-    {
-        MPI_Send(data, 10, MPI_INT, 1, 0, MPI_COMM_WORLD);
-    }
-    else
-        f(myrank == 1)
-        {
-            MPI_Recv(data, 10, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
     MPI_Finalize();
     return 0;
 }
